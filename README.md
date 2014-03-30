ffdd348c75982624bb87b364b6ad1e70eb68d492
Gerard Ketuma <gketuma@gmail.com>
2014-03-30 18:12:22 -0500

    Add all the rspec testing to our new rails app

---
+++ README.md
@@ -0,0 +1,4 @@
+Setup Rspec for new Rails Project
+=================================
+
+Example rails app showing how to include Rspec testing in a new Rails 4.0 app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rspec::Application.config.secret_key_base = '1936380d9ccefcd8c62a021ba5cd657bef87070f1ab8d85d737907153fc1f5339e8b306a4ba108ccefafdaf466fd38f1cc0f94b7973e65375a355c6550dc8ab6'
